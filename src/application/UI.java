853f2fe72d4fd86871eea02dcb2e74186f0eabc7
Gustavo Sthel <gustavosthel@gmail.com>
2024-01-14 13:20:56 -0300

    Special move - Promotion

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,11 +1,10 @@
 package application;
 
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import chess.ChessMatch;
